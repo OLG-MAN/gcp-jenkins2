75491a865a04eb395559afb816d7cf9145d29be2
oleg_mandrychenko <oleg_mandrychenko@epam.com>
2021-10-06 13:53:58 +0000

    move logic

--- vars/ping.groovy
+++ vars/ping.groovy
@@ -1,21 +1,19 @@
 #!/usr/bin/env groovy
 
 def call() {
-    parallel {
-        stage('Ping 1') {
-            steps {
-                sh "ping -c 2 1688.com"
-            }
+    stage('Ping 1') {
+        steps {
+            sh "ping -c 2 1688.com"
         }
-        stage('Ping 2') {
-            steps {
-                sh "ping -c 2 2ip.me"
-            }
+    }
+    stage('Ping 2') {
+        steps {
+            sh "ping -c 2 2ip.me"
         }
-        stage('Ping 3') {
-            steps {
-                sh "ping -c 2 8.8.8.8"
-            }
+    }
+    stage('Ping 3') {
+        steps {
+            sh "ping -c 2 8.8.8.8"
         }
     }
 }

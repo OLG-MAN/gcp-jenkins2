39a6879bf1eda0b2f7e9b0f5ef6065e880613b81
oleg_mandrychenko <oleg_mandrychenko@epam.com>
2021-10-07 06:57:37 +0000

    move logic

--- vars/ping.groovy
+++ vars/ping.groovy
@@ -2,11 +2,7 @@
 
 
 def call(String addr = 'google.com') {
-   script{
-      sh '''
-   PING_ADDR="8.8.8.8"
-   ping -c 2 ${PING_ADDR}"
-   '''
-   }
+      sh "PING_ADDR=8.8.8.8"
+      sh "ping -c 2 ${PING_ADDR}"
 }
 
